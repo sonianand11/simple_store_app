6af94b3184cce7c59ccb69845143cfd3c9761765
Anand Soni <sonianand11@gmail.com>
2016-02-27 15:16:32 +0530

    removing unwanted content from routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,4 @@ Rails.application.routes.draw do
   resources :products
   resources :stores
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
 end
