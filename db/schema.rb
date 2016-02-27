ecf559b42be8de7c8db4fe18ca295e719a949bad
Anand Soni <sonianand11@gmail.com>
2016-02-27 15:02:06 +0530

    adding products scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160227092445) do
+ActiveRecord::Schema.define(version: 20160227092838) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.float    "price"
+    t.integer  "store_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "stores", force: :cascade do |t|
     t.string   "name"
