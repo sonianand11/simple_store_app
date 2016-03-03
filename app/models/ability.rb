8fc1d97f7d4f25222861abde3281a8e974160c47
Anand Soni <sonianand11@gmail.com>
2016-03-03 12:14:03 +0530

    managing ability for users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,18 +4,22 @@ class Ability
   def initialize(user)
 
     user ||= User.new # guest user (not logged in)
+
     if user.has_role? :admin
       can :manage, :all
+
     elsif user.has_role? :user
       can :read, :all
       can :create, :all
       can :update, Store, user_id: user.id
-      can :update, Product, user_id: user.id
+      can :update, Product, store_id: user.store_ids
 
       can :destroy, Store, user_id: user.id
-      can :destroy, Product, user_id: user.id
+      can :destroy, Product, store_id: user.store_ids
+
     else
       can :read, :all
+      
     end    
 
   end
