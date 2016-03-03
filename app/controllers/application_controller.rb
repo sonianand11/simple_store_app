7ab8439eae7f257a9f440195f0947d66a761a490
Anand Soni <sonianand11@gmail.com>
2016-03-03 12:13:40 +0530

    catching cancan exception globally

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  include CanCan::ControllerAdditions  
+
+  rescue_from CanCan::AccessDenied do |exception|
+    render json: {error: exception.message}
+  end
+
+  def authenticate
+    request_http_basic_authentication if !(@current_user = authenticate_with_http_basic { |u, p|  User.authenticate(u,p)})
+  end
+
+  def current_user
+    @current_user
+  end
 end
