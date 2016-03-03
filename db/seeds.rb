47b06f21fb279df11008199edbd3918a0a83bbe7
Anand Soni <sonianand11@gmail.com>
2016-03-03 17:36:50 +0530

    changing way to populating data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Role.create([{name: "admin"},{name: "user"}])
+p 'Populating roles'
 
-admin_user = User.create(name: "admin",email: "admin@example.com",username: "admin", password: "admin",roles: [Role.admin])
-user = User.create(name: "user",email: "user@example.com",username: "user", password: "user",roles: [Role.user])
+['admin','user'].each do |name|
+  Role.find_or_create_by(name: name)
+end
 
-store = Store.create(name: "fake store",address: 'fake address',user_id: user.id)
+p 'Populating admin user'
+admin_user = User.find_or_create_by(email: 'admin@example.com') do |u|
+  u.name= 'admin'
+  u.username= 'admin'
+  u.password = 'admin'
+  u.roles =  [Role.admin]
+end
 
+p 'Populating user'
+user = User.find_or_create_by(email: 'user@example.com') do |u|
+  u.name= 'user'
+  u.username= 'user'
+  u.password = 'user'
+  u.roles =  [Role.user]
+end
+
+p 'Populating store'
+store = Store.first_or_create(user_id: user.id) do |s|
+  s.name= 'fake store'
+  s.address = 'fake address'
+end
+
+p 'Populating products'
 5.times do |i|
   Product.create(name: "Product#{i}",price: 100,store_id: store.id)
 end
