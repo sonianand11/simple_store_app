a469176b9a2aa7f9983c3854a1489bbfc2f36d0f
Anand Soni <sonianand11@gmail.com>
2016-03-04 18:43:23 +0530

    fix url issue in users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -20,7 +20,7 @@ class Api::V1::UsersController < Api::V1::BaseController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: @user, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -48,6 +48,6 @@ class Api::V1::UsersController < Api::V1::BaseController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email,:username,:password)
     end
 end
